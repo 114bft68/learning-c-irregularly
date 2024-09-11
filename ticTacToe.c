662ff94eabdc7ad323e834387e9d74b802e99673
114bft68 <168547332+114bft68@users.noreply.github.com>
2024-09-11 13:32:45 +0800

    tic tac toe

--- ticTacToe.c
+++ ticTacToe.c
@@ -3,15 +3,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-char board[] = "| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |", input;
+char board[] = "| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |", players[2][5] = { {}, {} }, input;
 
 void boardStuff(), win(char first, char second, char third);
 
-char players[2][5] = { {}, {} };
-
 int indices[2] = {0, 0};
 
-bool turn = false, end = false;
+bool turn = end = false;
 
 int main() {
     printf("~ Tic Tac Toe ~\n\n%s\n", board);
@@ -26,12 +24,8 @@ void boardStuff() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {} // buffer clearing thing, I don't really know what it is but I'll learn about that later
     
-    if (input > '0' && input <= '9') { // I want regex
-        if (turn) {
-            players[1][indices[1]++] = input;
-        } else {
-            players[0][indices[0]++] = input;
-        }
+    if (input > '0' && input <= '9') {
+        players[turn ? 1 : 0][indices[turn ? 1 : 0]++] = input;
         for (int i = 0; i < strlen(board); i++) {
             if (board[i] == input) {
                 board[i] = turn ? 'O' : 'X';
@@ -39,7 +33,14 @@ void boardStuff() {
         }
         turn = !turn;
         printf("\n%s\n", board);
-        win('1', '2', '3'); win('4', '5', '6'); win('7', '8', '9'); win('1', '4', '7'); win('2', '5', '8'); win('3', '6', '9'); win('1', '5', '9'); win('3', '5', '7');
+        win('1', '2', '3');
+        win('4', '5', '6');
+        win('7', '8', '9');
+        win('1', '4', '7');
+        win('2', '5', '8');
+        win('3', '6', '9');
+        win('1', '5', '9');
+        win('3', '5', '7');
         boardStuff();
     } else {
         boardStuff();
