fbb9d607adcbe745b9578b9a41d66ca2274c3da4
114bft68 <168547332+114bft68@users.noreply.github.com>
2024-10-17 19:03:48 +0800

    a simple empty window in c with <windows.h>

---
+++ window.c
@@ -0,0 +1,83 @@
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+
+const wchar_t CLASS_NAME[] = L"MyWindow";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASSEXW wc = {};
+
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.style = 0;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = NULL;
+    wc.hCursor = NULL;
+    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hIconSm = NULL;
+
+    if (!RegisterClassExW(&wc)) {
+        MessageBox(
+            NULL,
+            L"Failed to Register Class",
+            L"FAILURE",
+            MB_OK
+        );
+        return 0;
+    }
+
+    HWND hwnd;
+
+    hwnd = CreateWindowExW(
+        WS_EX_CLIENTEDGE,
+        CLASS_NAME,
+        L"An Ordinary Window",
+        WS_OVERLAPPEDWINDOW,
+        0,
+        0,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        MessageBox(
+            NULL,
+            L"Failed to Create Window",
+            L"FAILURE",
+            MB_OK
+        );
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg;
+    
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
