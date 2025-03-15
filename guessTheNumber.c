053325b1e20e088b5c2fbfd0d6150d47ccb857e9
114bft68 <168547332+114bft68@users.noreply.github.com>
2025-03-15 15:17:36 +0800

    make it slightly better

--- guessTheNumber.c
+++ guessTheNumber.c
@@ -3,27 +3,34 @@
 #include <time.h>
 #include <math.h>
 
-int number, input, lives = 10;
-
 int main() {
+    int number, input, lives = 10;
+
     srand(time(NULL));
     number = rand() % 100 + 1;
-    printf("~ Guess the Number from 1 to 100 ~\n~ You have 10 lives in total ~\n\n");
-    printf("Make your first guess: ");
+    
+    printf("~ Guess the Number from 1 to 100 ~\n~ You have 10 lives in total ~\n\nMake your first guess: ");
+    
     while (lives > 0) {
+        printf(": ");
         scanf("%d", &input);
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {}
+
+        lives--;
+        
         if (input == number) {
-            printf("\nYou won!");
-            lives = 0;
+            
+            puts("\nYou won!");
+            return 0;
+        
         } else {
-            if (lives > 1) {
-                lives--;
-                printf("The number is %s than your previous guess: ", number > input ? "higher" : "lower");
-            } else {
-                printf("\nThe number was %d", number);
-                lives = 0;
-            }
+
+            printf("The number is %s than your previous guess", number > input ? "higher" : "lower ");
+        
         }
     }
+
+    puts("\nYou lost");
     return 0;
 }
